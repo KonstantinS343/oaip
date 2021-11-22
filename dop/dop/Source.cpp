a7a7d336bfbe71402f09c994e5043d4a4a778ca6
KonstantinS343 <mihailrudkovic@gmail.com>
2021-11-22 17:40:53 +0300

    last

--- dop/dop/Source.cpp
+++ dop/dop/Source.cpp
@@ -18,36 +18,60 @@ vector<string> convert(vector<string>  str) {
 		int size = 0, sizes, obrazec;
 		vector<string> stroka;
 		vector<string> strokav2;
-		vector<int>index;
+		int index;
+		string str;
+		string word;
 		int lenth;
 		cout << "¬ведите строку:";
-		stroka.resize(1000);
-		for (int i = 0; i < 1000; i++) {
-			cin >> stroka[i];
-			size++;
-			if (stroka[i] == ".") {
-				break;
+		getline(cin, str);
+		char space = ' ';
+		for (int i = 0; i < str.size(); i++) {
+			if (str[i] == ' ') {
+				
+					word = str.substr(0, i);
+					stroka.push_back(word);
+;					str.erase(0, i + 1);
+				
+				i = 0;
 			}
 		}
-		stroka.resize(size);
+		stroka.push_back(str);
 		strokav2 = convert(stroka);
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < stroka.size(); i++) {
 			cout << stroka[i];
 			cout << " ";
 		}
 		cout << "\n";
-		for (int i = 0; i < size; i++) {
+		int y = stroka.size();
+		for (int i = 0; i < y; i++) {
 			obrazec = i;
-			for (int k = 1; k < strokav2.size(); k++) {
+			for (int k = i+1; k < strokav2.size(); k++) {
 				if (strokav2[i][0] == strokav2[k][0]) {
 					for (int counter = 0; counter < strokav2[i].size() - 1; counter++) {
-						if (counter == strokav2[k].size()) {
+						if (strokav2[i].size() > strokav2[k].size()) {
+							break;
+						}
+						if (strokav2[k].size() > strokav2[i].size()) {
 							break;
 						}
 						if (strokav2[i][counter] == strokav2[k][counter]) {
 							if (counter == strokav2[k].size() - 2) {
-								index.push_back(k);
-								size = 1;
+								index=k;
+								int lenobr = stroka[obrazec].size();
+								
+									int lenth = stroka[index].size();
+									if (stroka[index][lenth - 1] == toupper(stroka[index][lenth - 1])) {
+										stroka[index][lenth - 1] = toupper(stroka[obrazec][lenobr - 1]);
+									}
+									else {
+										if (stroka[obrazec][lenobr - 1] == toupper(stroka[obrazec][lenobr - 1]) && stroka[index][lenth - 1] == tolower(stroka[index][lenth - 1])) {
+											stroka[index][lenth - 1] = tolower(stroka[obrazec][lenobr - 1]);
+										}
+										else {
+											stroka[index][lenth - 1] = stroka[obrazec][lenobr - 1];
+										}
+									}
+									strokav2 = convert(stroka);
 							}
 						}
 					}
@@ -55,25 +79,12 @@ vector<string> convert(vector<string>  str) {
 			}
 
 		}
-		int lenobr = stroka[obrazec].size();
-		for (int i = 0; i < index.size(); i++) {
-			int lenth = stroka[index[i]].size();
-			if (stroka[index[i]][lenth - 1]==toupper(stroka[index[i]][lenth - 1])) {
-				stroka[index[i]][lenth - 1] = toupper(stroka[obrazec][lenobr - 1]);
-			}
-			else {
-				if (stroka[obrazec][lenobr - 1]==toupper(stroka[obrazec][lenobr - 1]) && stroka[index[i]][lenth - 1]==tolower(stroka[index[i]][lenth - 1])) {
-					stroka[index[i]][lenth - 1] =tolower( stroka[obrazec][lenobr - 1]);
-				}
-				else {
-					stroka[index[i]][lenth - 1] = stroka[obrazec][lenobr - 1];
-				}
-			}
-		}
+		string answer;
 		for (int i = 0; i < stroka.size(); i++) {
-			cout << stroka[i];
-			cout << " ";
+			answer+=stroka[i];
+			answer+=" ";
 		}
+		cout << answer;
 
 		return 0;
 	}
